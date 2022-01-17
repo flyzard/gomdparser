37a0d46a82eac0b31b96404c6b027a53a79a4bfa
Joao Felizardo <felizardoj>
2022-01-17 16:12:52 +0000

    Add default arguments and possibility to choose the number of releases

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 !! This code is meant to be used in a very specific project where the release notes are meant to be parsed from MarkDown to HTML and it expects a file `release_notes.md`, generating a new file in `www/LatestReleaseNotes.html`
 
-### Just run `gomdparser` on the project base directory
+### How to run
+`./gomdparser path_to_releases.md path_to_new_release.html path_to_latest_releases.html n_latest_releases_int`
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/gomarkdown/markdown"
@@ -11,25 +12,72 @@ import (
 
 func main() {
 
-    markdownFilePath := os.Args[1]
-    htmlFilePath := os.Args[2]
+    markdownFilePath, latestReleaseHtmlFilePath, htmlFilePath, nReleases := getArguments(os.Args)
 
     content, err := ioutil.ReadFile(markdownFilePath)
     if err != nil {
         log.Fatal(err)
     }
 
+    // Convert the full release notes file to MD
+    convertContentToMd(content, htmlFilePath)
+    
     // Get only the latest release, given that they are separated by ##
-    lenght := strings.Index(string(content)[2:], "##")
+    lenght := strings.Index(string(content)[nReleases + 1:], "##")
     content = content[0:lenght]
-	html := markdown.ToHTML(content, nil, nil)
 
-	f, err := os.OpenFile(htmlFilePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+    // Convert the nReleases to html
+	convertContentToMd(content, latestReleaseHtmlFilePath)
+}
+
+func getArguments(givenArgs []string) (string, string, string, int) {
+
+    // default names when no arguments passed
+    args := [3]string{"release_notes.md", "www/latest_release.html", "www/releases.html" }
+
+    size := len(givenArgs)
+
+   for i := 1; i < 4; i++ {
+       if size >= i+1 {
+           args[i-1] = givenArgs[i]
+       }
+
+       if i == 1 {
+            if _, err := os.Stat(args[i-1]); err != nil {
+                log.Fatal(err)
+            }
+        }
+   }
+
+    // for i := range args {
+    //     if size >= i+2 {
+    //         args[i] = givenArgs[i+1]
+    //     }
+
+    //     if i == 1 {
+    //         if _, err := os.Stat(args[i]); err != nil {
+    //             log.Fatal(err)
+    //         }
+    //     }
+    // }
+
+    nReleases := 1
+    if size > 4 {
+        nReleases, _ = strconv.Atoi(givenArgs[4])
+    }
+
+    return args[0], args[1], args[2], nReleases
+}
+
+
+func convertContentToMd(content []byte, filPath string) {
+    html := markdown.ToHTML(content, nil, nil)
+    f, err := os.OpenFile(filPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
     if err != nil {
         log.Fatal(err)
     }
 
-	f.WriteAt(html, 0)
+    f.WriteAt(html, 0)
 
     if err := f.Close(); err != nil {
         log.Fatal(err)
